991d2c6ec6a6e71adfdd81a92393580640c19e08
fire3420 <fire3420@gmail.com>
2020-04-21 01:20:03 +0900

    modified to setup.sh

--- jupyterlab/setup.sh
+++ jupyterlab/setup.sh
@@ -1,3 +1,15 @@
+apt-get update 
+apt-get install -y tmux
+apt-get install -y vim
+apt-get install -y npm
+
+
+curl -sL https://deb.nodesource.com/setup_12.x | sh
+apt-get install -y nodejs 
+yes | pip install jupyterlab
+apt-get install -y git
+
+
 # VIM
 jupyter labextension install @axlair/jupyterlab_vim
 jupyter labextension enable @axlair/jupyterlab_vim
