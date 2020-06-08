d1e477b536f3fe20aa93d92668f452b1f2931d6d
john <fire3420@gmail.com>
2020-06-08 22:42:42 +0900

    added code insider shortcuts

--- Autokey/Autokey/hyper_slash.py
+++ Autokey/Autokey/hyper_slash.py
@@ -1,4 +1,4 @@
 winClass = window.get_active_class()
 
-if 'code.Code' in winClass or 'vscodium.VSCodium' in winClass:
+if 'code.Code' in winClass or 'vscodium.VSCodium' in winClass or 'code - insiders.Code - Insiders' in winClass:
     keyboard.send_keys('<ctrl>+<shift>+<alt>+f')
