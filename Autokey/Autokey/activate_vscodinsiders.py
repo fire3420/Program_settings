635424f9e92ea3b47fc3c2041095b8deb29008cb
john <fire3420@gmail.com>
2020-06-09 00:37:54 +0900

    revised vscode insider activation

--- Autokey/Autokey/activate_vscodinsiders.py
+++ Autokey/Autokey/activate_vscodinsiders.py
@@ -1,16 +1,23 @@
 # Enter script code
 import subprocess
 import os
+import socket
 
-def winexists(target, nontarget):
+def winexists(target, nontarget, hostname=socket.gethostname()):
     getexists = []
     for line in subprocess.check_output(['wmctrl', '-l']).splitlines():
         window_name = line.split(None, 3)[-1].decode()
-        if target in window_name and not(nontarget in window_name):
-            getexists.append(line.split(None, 3)[0].decode())
+        window_host = line.split(None, 3)[2].decode()
+        if target in window_name and hostname in window_host:
+            check_app = True
+            for ni in nontarget:
+                if ni in window_name:
+                    check_app = False
+            if check_app:
+                getexists.append(line.split(None, 3)[0].decode())
     return getexists
 
-getlist = winexists("Code - Insiders", "Chrome")
+getlist = winexists("Code - Insiders", ["Chrome"])
 
 as3_index_insiders = store.get_value("as3_index_insiders")
 if(not as3_index_insiders):
